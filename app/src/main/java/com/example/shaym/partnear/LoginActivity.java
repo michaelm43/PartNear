b2207a72dbfab3f100ead114f23903b33ce9361a
michaelm43 <michaelm43@gmail.com>
2019-03-05 18:57:31 +0200

    second

--- app/src/main/java/com/example/shaym/partnear/LoginActivity.java
+++ app/src/main/java/com/example/shaym/partnear/LoginActivity.java
@@ -9,7 +9,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+///////////////////////////////////////////////////////////////////////////////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
