dac9b2ce9dd2058bc991501a1316ae210b624a85
michaelm43 <afeka2017>
2019-03-20 11:04:40 +0200

    Fragments_2

---
+++ app/src/main/java/com/example/shaym/partnear/ActivityListFragment.java
@@ -0,0 +1,377 @@
+package com.example.shaym.partnear;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.shaym.partnear.Adapters.ActivityRecyclerAdapter;
+import com.example.shaym.partnear.Logic.Activity;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.example.shaym.partnear.Util.Constants.ERROR_DIALOG_REQUEST;
+import static com.example.shaym.partnear.Util.Constants.MAPVIEW_BUNDLE_KEY;
+import static com.example.shaym.partnear.Util.Constants.PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
+import static com.example.shaym.partnear.Util.Constants.PERMISSIONS_REQUEST_ENABLE_GPS;
+
+public class ActivityListFragment extends Fragment  implements View.OnClickListener,
+    ActivityRecyclerAdapter.ActivityRecyclerClickListener {
+
+    private static final String TAG = "ActivityListFragment";
+
+    //widgets
+    private ProgressBar mProgressBar;
+
+    //vars
+    private ArrayList<Activity> activities = new ArrayList<>();
+    private Set<String> activitiesIDs = new HashSet<>();
+    private ActivityRecyclerAdapter activityRecyclerAdapter;
+    private RecyclerView activityRecyclerView;
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore mDb;
+    private ListenerRegistration activityEventListener;
+
+    private boolean mLocationPermissionGranted = false;
+
+    private Context context;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_activity_list, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        activityRecyclerView = (RecyclerView) view.findViewById(R.id.activities_recycler_view);
+
+        mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+
+        getActivity().findViewById(R.id.fab_create_activity).setOnClickListener(this);
+
+        getActivity().findViewById(R.id.fab_map).setOnClickListener(this);
+
+        mDb = FirebaseFirestore.getInstance();
+
+        initActivityRecyclerView();
+
+        context = container.getContext();
+
+        return view;
+    }
+    private boolean checkMapServices(){
+        if(isServicesOK()){
+            if(isMapsEnabled()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void buildAlertMessageNoGps() { // Show a dialog to enable GPS
+        final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage("This application requires GPS to work properly, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        Intent enableGpsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivityForResult(enableGpsIntent, PERMISSIONS_REQUEST_ENABLE_GPS);
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public boolean isMapsEnabled(){ // determine if the GPS is on or off
+        final LocationManager manager = (LocationManager) getActivity().getSystemService( Context.LOCATION_SERVICE );
+
+        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps();
+            return false;
+        }
+        return true;
+    }
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+            getActivities();
+        } else {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    public boolean isServicesOK(){ // determine if there is Google Services on the device and is usable
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(context);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(getActivity(), available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(context, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) { // What happened after the dialog permission
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: called.");
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ENABLE_GPS: {
+                if(mLocationPermissionGranted){
+                    getActivities(); // if accepted the GPS dialog
+                }
+                else{
+                    getLocationPermission();
+                }
+            }
+        }
+
+    }
+
+    private void initActivityRecyclerView(){
+        activityRecyclerAdapter = new ActivityRecyclerAdapter(activities, this);
+        activityRecyclerView.setAdapter(activityRecyclerAdapter);
+        activityRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getActivities();
+        if(checkMapServices()){
+            if(mLocationPermissionGranted)
+                getActivities();
+            else
+                getLocationPermission();
+        }
+    }
+
+    @Override
+    public void onActivitySelected(int position) {
+        navActivityActivity(activities.get(position));
+    }
+
+    private void navActivityActivity(Activity activity){
+        Intent intent = new Intent(context, ActivityActivity.class);
+        intent.putExtra(getString(R.string.intent_activity), activity);
+        //startActivity(intent);
+    }
+
+    private void newActivityDialog(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Enter a Activity name");
+
+        final EditText input = new EditText(context);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        builder.setPositiveButton("CREATE", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(!input.getText().toString().equals("")){
+                    buildNewActivity(input.getText().toString());
+                }
+                else {
+                    Toast.makeText(context, "Enter an activity name", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void getActivities(){
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setTimestampsInSnapshotsEnabled(true)
+                .build();
+        mDb.setFirestoreSettings(settings);
+
+        CollectionReference activitiesCollection = mDb
+                .collection(getString(R.string.collection_activities));
+
+        activityEventListener = activitiesCollection.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                Log.d(TAG, "onEvent: called.");
+
+                if (e != null) {
+                    Log.e(TAG, "onEvent: Listen failed.", e);
+                    return;
+                }
+
+                if(queryDocumentSnapshots != null){
+                    for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
+
+                        Activity activity = doc.toObject(Activity.class);
+                        if(!activitiesIDs.contains(activity.getActivityID())){
+                            activitiesIDs.add(activity.getActivityID());
+                            activities.add(activity);
+                        }
+                    }
+                    Log.d(TAG,"onEvent: number of chatrooms: " + activities.size());
+                    activityRecyclerAdapter.notifyDataSetChanged();
+                }
+
+            }
+        });
+    }
+
+    private void buildNewActivity(String type){
+
+        final Activity activity = new Activity(type);
+
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setTimestampsInSnapshotsEnabled(true)
+                .build();
+        mDb.setFirestoreSettings(settings);
+
+        DocumentReference newActivityRef = mDb
+                .collection(getString(R.string.collection_activities))
+                .document();
+
+        activity.setActivityID(newActivityRef.getId());
+
+        newActivityRef.set(activity).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                hideDialog();
+
+                if(task.isSuccessful()){
+                    //navActivityActivity(activity);
+                }else{
+                    View parentLayout = getView().findViewById(android.R.id.content);
+                    Snackbar.make(parentLayout, "Something went wrong.", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void showDialog(){
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideDialog(){
+        mProgressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(activityEventListener != null){
+            activityEventListener.remove();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+
+            case R.id.fab_create_activity:{
+                newActivityDialog();
+                break;
+            }
+            case R.id.fab_map:{
+                ActivityListAndMapFragment fragment = ActivityListAndMapFragment.newInstance();
+                Bundle bundle = new Bundle();
+                bundle.putParcelableArrayList(getString(R.string.intent_activity_list), activities);
+                fragment.setArguments(bundle);
+
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.setCustomAnimations(R.anim.slide_in_up, R.anim.slide_out_up);
+                transaction.replace(R.id.fragmentContainer, fragment, getString(R.string.fragment_activity_list));
+                transaction.addToBackStack(getString(R.string.fragment_activity_list));
+                transaction.commit();
+                break;
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/shaym/partnear/MainActivity.java
+++ app/src/main/java/com/example/shaym/partnear/MainActivity.java
@@ -1,62 +1,25 @@
 package com.example.shaym.partnear;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.text.InputType;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.support.v7.widget.Toolbar;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import com.example.shaym.partnear.Adapters.ActivityRecyclerAdapter;
-import com.example.shaym.partnear.Logic.Activity;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.FirebaseFirestoreSettings;
-import com.google.firebase.firestore.ListenerRegistration;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements
-        View.OnClickListener,
-        ActivityRecyclerAdapter.ActivityRecyclerClickListener {
+    private static final String TAG = "MainActivity";
 
-    //widgets
-    private ProgressBar mProgressBar;
-
-    //vars
-    private ArrayList<Activity> activities = new ArrayList<>();
-    private Set<String> activitiesIDs = new HashSet<>();
-    private ActivityRecyclerAdapter activityRecyclerAdapter;
-    private RecyclerView activityRecyclerView;
     private Toolbar mainToolbar;
 
     private FirebaseAuth mAuth;
-    private FirebaseFirestore mDb;
-    private ListenerRegistration activityEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,31 +27,27 @@ public class MainActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_main);
         //EditText et = (EditText) findViewById(R.id.etSearch);
 
+        if (findViewById(R.id.fragmentContainer) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+            ActivityListFragment firstFragment = new ActivityListFragment();
+
+            firstFragment.setArguments(getIntent().getExtras());
+
+            getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, firstFragment).commit();
+        }
+
         mAuth = FirebaseAuth.getInstance();
 
         mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(mainToolbar);
 
-        activityRecyclerView = (RecyclerView) findViewById(R.id.activities_recycler_view);
-
-        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
-
-        findViewById(R.id.fab_create_activity).setOnClickListener(this);
-
-        mDb = FirebaseFirestore.getInstance();
-
         initSupportActionBar();
-        initActivityRecyclerView();
     }
 
     private void initSupportActionBar(){
         setTitle(R.string.app_name);
-    }
-
-    private void initActivityRecyclerView(){
-        activityRecyclerAdapter = new ActivityRecyclerAdapter(activities, this);
-        activityRecyclerView.setAdapter(activityRecyclerAdapter);
-        activityRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
@@ -139,144 +98,5 @@ public class MainActivity extends AppCompatActivity implements
         Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
         MainActivity.this.startActivity(loginIntent);
         finish();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        getActivities();
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-
-            case R.id.fab_create_activity:{
-                newActivityDialog();
-            }
-        }
-    }
-
-    @Override
-    public void onActivitySelected(int position) {
-        navActivityActivity(activities.get(position));
-    }
-
-    private void navActivityActivity(Activity activity){
-        Intent intent = new Intent(MainActivity.this, ActivityActivity.class);
-        intent.putExtra(getString(R.string.intent_activity), activity);
-        //startActivity(intent);
-    }
-
-    private void newActivityDialog(){
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Enter a Activity name");
-
-        final EditText input = new EditText(this);
-        input.setInputType(InputType.TYPE_CLASS_TEXT);
-        builder.setView(input);
-
-        builder.setPositiveButton("CREATE", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                if(!input.getText().toString().equals("")){
-                    buildNewActivity(input.getText().toString());
-                }
-                else {
-                    Toast.makeText(MainActivity.this, "Enter an activity name", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-
-        builder.show();
-    }
-
-    private void getActivities(){
-        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
-                .setTimestampsInSnapshotsEnabled(true)
-                .build();
-        mDb.setFirestoreSettings(settings);
-
-        CollectionReference activitiesCollection = mDb
-                .collection(getString(R.string.collection_activities));
-
-        activityEventListener = activitiesCollection.addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                //Log.d(TAG, "onEvent: called.");
-
-                if (e != null) {
-                    //Log.e("onEvent: Listen failed.", e);
-                    return;
-                }
-
-                if(queryDocumentSnapshots != null){
-                    for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
-
-                        Activity activity = doc.toObject(Activity.class);
-                        if(!activitiesIDs.contains(activity.getActivityID())){
-                            activitiesIDs.add(activity.getActivityID());
-                            activities.add(activity);
-                        }
-                    }
-                    //Log.d("onEvent: number of chatrooms: " + activities.size());
-                    activityRecyclerAdapter.notifyDataSetChanged();
-                }
-
-            }
-        });
-    }
-
-    private void buildNewActivity(String type){
-
-        final Activity activity = new Activity(type);
-
-        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
-                .setTimestampsInSnapshotsEnabled(true)
-                .build();
-        mDb.setFirestoreSettings(settings);
-
-        DocumentReference newActivityRef = mDb
-                .collection(getString(R.string.collection_activities))
-                .document();
-
-        activity.setActivityID(newActivityRef.getId());
-
-        newActivityRef.set(activity).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                hideDialog();
-
-                if(task.isSuccessful()){
-                    //navActivityActivity(activity);
-                }else{
-                    View parentLayout = findViewById(android.R.id.content);
-                    Snackbar.make(parentLayout, "Something went wrong.", Snackbar.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-
-    private void showDialog(){
-        mProgressBar.setVisibility(View.VISIBLE);
-    }
-
-    private void hideDialog(){
-        mProgressBar.setVisibility(View.GONE);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if(activityEventListener != null){
-            activityEventListener.remove();
-        }
     }
 }
